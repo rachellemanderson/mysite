554bd9339c22022b33e3c28a7db3287937301e87
Rachelle Dietz <rachelledietz@Rachelle-Dietzs-MacBook-Pro.local>
2016-05-19 19:55:43 -0600

    new home

--- wp-content/themes/rachelle/page-home.php
+++ wp-content/themes/rachelle/page-home.php
@@ -10,14 +10,90 @@ get_header(); ?>
 <div class="home-page">
 	<header class="page-header">
 	</header>
-	<div class="container">
-		<div class="logo-container">
-			<img src="<?php echo get_template_directory_uri () ?>/assets/img/rachelle.png" class="picture" alt="Rachelle Anderson"/>
-			<img src="<?php echo get_template_directory_uri () ?>/assets/img/rachelle-text.png" class="logo" alt="Rachelle Anderson"/>
+	<section class="container work-archive">
+		<h2>My Work</h2>
+		<div class="inner-container page-content">
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-ouai.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>theouai.com</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-bluestar.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>bluestarcooking.com</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-case.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>case-agency.com</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-fightnight.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>fightnight.vision</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-bluestar.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>bluestarcooking.com</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="tile">
+				<img src="<?php echo get_template_directory_uri () ?>/assets/img/work-case.jpg" class="picture" alt="Rachelle Anderson"/>
+				<div class="tile-overlay"></div>
+				<div class="over-text">
+					<div class="outer-table">
+						<div class="inner-table">
+							<p>case-agency.com</p>
+						</div>
+					</div>
+				</div>
+			</a>
 		</div>
-	</div>
-	<section class="container page-content">
-
+	</section>
+	<section class="container experience">
+		<div class="heading-wrapper">
+			<h2>Experience</h2>
+		</div>
+		<div class="third">
+			<img src="<?php echo get_template_directory_uri () ?>/assets/img/byu.png" alt="BYU"/>
+		</div>
+		<div class="third">
+			<img src="<?php echo get_template_directory_uri () ?>/assets/img/qualtrics.png" class="qualtrics" alt="Qualtrics"/>
+		</div>
+		<div class="third">
+			<img src="<?php echo get_template_directory_uri () ?>/assets/img/voltage.png" alt="Voltage"/>
+		</div>
 	</section>
 </div>
 
